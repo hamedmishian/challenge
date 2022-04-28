a21350db4080940983419f2c57cb4aef8b299d7f
hamedmishian <hamedmishian@gmail.com>
2022-04-28 14:48:34 +0430

    bb

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -10,14 +10,16 @@ function Page() {
   const [pathname, setPathname] = useState(location?.pathname);
 
   useEffect(() => {
-    fetch("http://localhost:3000/data")
+    fetch(
+      "https://dataguard.blob.core.windows.net/challenges/plugins/fe-challenge.json"
+    )
       .then(res => res.json())
       .then(res => setData(res))
       .catch(error => console.log(error));
   }, []);
 
-  const tabData = data && Object.values(data?.tabdata);
-  const plugins = data && Object.values(data?.plugins);
+  const tabData = data && Object.values(data?.data?.tabdata);
+  const plugins = data && Object.values(data?.data?.plugins);
 
   const activeMarketing = tabData?.filter(
     item => item?.title === "Marketing"
